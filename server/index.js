7f3190687034625b9f54f74dcea3127717b6487d
Talis Lazdins <lazdinst@gmail.com>
2017-10-24 11:20:30 -0700

    Updated Default Port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log('listening on port ' + port + '!');
